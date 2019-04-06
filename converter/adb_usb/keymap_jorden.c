b3d30aaf28c6206a1a9841e4b8de7e826a587353
Jorden Lowe <supermitsuba@gmail.com>
2019-04-06 06:08:06 -0400

    fixed end placement on layer 2

--- converter/adb_usb/keymap_jorden.c
+++ converter/adb_usb/keymap_jorden.c
@@ -24,21 +24,19 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,FN1,      DEL, END, PGDN,    P7,  P8,  P9,  PMNS,
     LCAP,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         P4,  P5,  P6,  PPLS,
     LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,          UP,           P1,  P2,  P3,
-    LCTL,LALT, FN2,          SPC,                          FN3, RALT,RCTL,     LEFT,DOWN,RGHT,    P0,       PDOT,PENT
+    LCTL,LALT,LGUI,          SPC,                         RGUI, RALT,RCTL,     LEFT,DOWN,RGHT,    P0,       PDOT,PENT
     ),
     [1] = KEYMAP(
     GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,           PSCR,SLCK,BRK,     VOLD,VOLU,MUTE,CALC,
     FN0, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,      INS, HOME,PGUP,    NLCK,EQL, PSLS,PAST,
     TAB, Q,   W,   E,   R,   T,CALC,PGUP,   UP,PGDN, PSCR,LBRC,RBRC, FN1,      DEL, END, PGDN,    P7,  P8,  P9,  PMNS,
     LCAP,A,   S,   D,   F,   G,HOME,LEFT, DOWN,RGHT,  SCLN,QUOT,     ENT,                         P4,  P5,  P6,  PPLS,
-    LSFT,NUBS,Z,   X,   C,   V, END, END,   M,   COMM,DOT, SLSH,     RSFT,          VOLU,         P1,  P2,  P3,
-    LCTL,LALT, FN2,          SPC,                          FN3, RALT,RCTL,     MPLY,VOLD,MUTE,    P0,       PDOT,BTLD
+    LSFT,NUBS,Z,   X,   C,   V,   B, END,  END,COMM,  DOT, SLSH,     RSFT,          VOLU,         P1,  P2,  P3,
+    LCTL,LALT,LGUI,          SPC,                         RGUI, RALT,RCTL,     MPLY,VOLD,MUTE,    P0,       PDOT,BTLD
     ),
 };
 
 const action_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_TAP_KEY(1, KC_GRV),
-    [1] = ACTION_LAYER_TAP_KEY(1, KC_BSLS),
-    [2] = ACTION_LAYER_TAP_KEY(1, KC_LGUI),
-    [3] = ACTION_LAYER_TAP_KEY(1, KC_RGUI),
+    [1] = ACTION_LAYER_TAP_KEY(1, KC_BSLS)
 };
